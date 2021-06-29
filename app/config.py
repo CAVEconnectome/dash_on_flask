fa7239e1c307ed0905186450a7daaeaf6b294e8a
Forrest Collman <forrest.collman@gmail.com>
2021-06-28 21:30:20 -0700

    changing app pattern

--- app/config.py
+++ app/config.py
@@ -1,18 +1,58 @@
 import os
-from dashconnectivityviewer import cell_type_connectivity, cell_type_table
+from dash_connectivity_viewer import (
+    cell_type_connectivity,
+    cell_type_table,
+    connectivity_table,
+)
+
+ct_config = {
+    "cell_type_dropdown_options": [
+        {
+            "label": "All Soma Prediction",
+            "value": "allen_soma_coarse_cell_class_model_v1",
+        },
+        {
+            "label": "Column Census (slanted)",
+            "value": "allen_v1_column_types_slanted",
+        },
+        {
+            "label": "Column Census (straight)",
+            "value": "allen_v1_column_types_v2",
+        },
+        {
+            "label": "Thalamic Axons",
+            "value": "allen_v1_column_thalamic",
+        },
+        {
+            "label": "Layer 5 IT PyC Subtypes",
+            "value": "allen_column_l5it_types",
+        },
+        {
+            "label": "Basket Subtypes",
+            "value": "allen_column_basket_molecular",
+        },
+    ],
+}
 
 
 class BaseConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
     DASH_DATASTACK_SUPPORT = {
-        "minnie65_phase3_v1": ["connectivity", "cell_type"],
-    }
-
-    DASH_APPS = {
-        "connectivity": {"create_app": cell_type_connectivity.create_app, "config": {}},
-        "cell_type": {
-            "create_app": cell_type_table.create_app,
-            "config": {},
+        "minnie65_phase3_v1": {
+            "cell_type": {
+                "create_app": cell_type_table.create_app,
+                "config": ct_config,
+            },
+            "connectivity": {
+                "create_app": cell_type_connectivity.create_app,
+                "config": ct_config,
+            },
+        },
+        "minnie65_public_v117": {
+            "connectivity_table": {
+                "create_app": connectivity_table.create_app,
+                "config": {},
+            }
         },
     }
 
--- app/webapp.py
+++ app/webapp.py
@@ -3,25 +3,30 @@ from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
-
+from middle_auth_client import auth_requires_permission
 
 from werkzeug.urls import url_parse
 
 __version__ = "0.0.1"
 
-server_bp = Blueprint('main', __name__)
+server_bp = Blueprint("main", __name__)
 
 
-@server_bp.route('/')
+@server_bp.route("/")
 def index():
     # show a list of datastacks in config
-    return render_template("index.html", title='Home Page', version=__version__)
+    return render_template("index.html", title="Home Page", version=__version__)
 
-@server_bp.route('/datastack/<datastack_name>')
+
+@server_bp.route("/datastack/<datastack_name>")
+@auth_requires_permission(
+    "view", table_arg="datastack_name", resource_namespace="datastack"
+)
 def datastack_view(datastack_name):
-    #show me a page with all apps supported by datastack
-    return render_template("datastack.html",
-                           datastack=datastack_name,
-                           title='Home Page',
-                           version=__version__)
-
+    # show me a page with all apps supported by datastack
+    return render_template(
+        "datastack.html",
+        datastack=datastack_name,
+        title="Home Page",
+        version=__version__,
+    )
