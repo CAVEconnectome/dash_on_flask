e1fb1789563d0c8964360309741e3c48652b0871
Forrest Collman <forrest.collman@gmail.com>
2022-02-03 14:58:08 -0800

    updating build docker script

--- build_docker.sh
+++ build_docker.sh
@@ -2,3 +2,4 @@
 docker build --no-cache -t gcr.io/neuromancer-seung-import/dashonflask:v$1 .
 docker push gcr.io/neuromancer-seung-import/dashonflask:v$1
 docker tag gcr.io/neuromancer-seung-import/dashonflask:v$1 docker.io/caveconnectome/dashonflask:v$1
+docker push docker.io/caveconnectome/dashonflask:v$1
