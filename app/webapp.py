a59ac84b4ccb032856831d45f9fec4c7802bde7b
Forrest Collman <forrest.collman@gmail.com>
2023-05-24 23:59:27 -0500

    Bump version: 1.11.2 → 1.12.0

--- app/webapp.py
+++ app/webapp.py
@@ -3,7 +3,7 @@ from flask import render_template
 from middle_auth_client import auth_requires_permission
 import os 
 
-__version__ = "1.11.2"
+__version__ = "1.12.0"
 
 server_bp = Blueprint("main", __name__)
 
