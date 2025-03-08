7c4fa3b81ed383877cd47214a7e6888d5fb0ba36
Forrest Collman <forrest.collman@gmail.com>
2025-03-08 09:03:29 -0800

    changing req building

--- compile-reqs.sh
+++ compile-reqs.sh
@@ -1,1 +1,2 @@
-docker run -v ${PWD}:/app caveconnectome/dashonflask:vgeneraldash1  /bin/bash -c "pip install pip-tools && pip-compile -v -r requirements.in"
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+docker run -v ${PWD}:/app python:3.10 /bin/bash -c "cd /app && pip install pip-tools && pip-compile -v -r requirements.in"
