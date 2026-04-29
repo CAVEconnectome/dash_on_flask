3862f4c94509548ca16191c49b05447de475d94d
Forrest Collman <forrest.collman@gmail.com>
2026-04-29 04:44:07 -0700

    fixing cloudbuild

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-docker build --no-cache -t gcr.io/neuromancer-seung-import/dashonflask:v$1 .
+DOCKER_BUILDKIT=1 docker build --no-cache -t gcr.io/neuromancer-seung-import/dashonflask:v$1 .
 docker push gcr.io/neuromancer-seung-import/dashonflask:v$1
 docker tag gcr.io/neuromancer-seung-import/dashonflask:v$1 docker.io/caveconnectome/dashonflask:v$1
 docker push docker.io/caveconnectome/dashonflask:v$1
